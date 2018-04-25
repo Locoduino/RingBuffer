ddbbf269c180bd6b88746d1e9fd96fe861d4da28
Koryphon <jean-luc@locoduino.org>
2018-04-25 18:10:18 +0200

    example wip

---
+++ examples/PushAndPop/PushAndPop.ino
@@ -0,0 +1,13 @@
+#include <RingBuffer.h>
+
+RingBuffer<uint8_t, 10> myBuffer;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
